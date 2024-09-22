a3a0679c04d3ff6a248bea4a5048555deaad3ddf
MG# <matthew.gream@pobox.com>
2024-09-22 09:17:14 +0200

    fix issue

--- server/http/server.js
+++ server/http/server.js
@@ -62,8 +62,8 @@ const image_dataManifest = (directory) => Object.values (fs.readdirSync (directo
     }, {}));
 
 xxx.get ('/images/images.json', async (req, res) => {
-	const url_base = `http://${host}:${port}/images/`;
-	const manifest = image_dataManifest (data_images).map (({ filename, ...rest }) => ({ ...rest, url: url_base + filename }));
+    const url_base = `http://${host}:${port}/images/`;
+    const manifest = image_dataManifest (data_images).map (({ filename, ...rest }) => ({ ...rest, url: url_base + filename }));
     console.log (`/images manifest request: ${manifest.length} items, ${JSON.stringify (manifest).length} bytes, types = ${manifest.map (item => item.type).join (', ')}`);
     res.json (manifest);
 });
@@ -71,24 +71,24 @@ xxx.put ('/images', image_upload.single ('image'), (req, res) => {
     if (!req.file) {
         console.error (`/images upload failed: file not provided`);
         return res.status (400).send ('File not provided');
-	}
+    }
     if (!req.file.originalname || !image_dataType (req.file.originalname) || !image_dataVersion (req.file.originalname)) {
         console.error (`/images upload failed: file has no name or has bad type/version (received '${req.file.originalname}')`);
         return res.status (400).send ('File has no name or bad type/version');
-	}
+    }
     if (fs.existsSync (path.join (data_images, req.file.originalname) + '.zz')) {
-		console.error (`/images upload failed: file already exists as '${uploadedName}'`);
-		return res.status (409).send ('File with this name already exists');
+        console.error (`/images upload failed: file already exists as '${path.join (data_images, req.file.originalname)}'`);
+        return res.status (409).send ('File with this name already exists');
      }
 
     try {
         const uploadedName = req.file.originalname, uploadedData = fs.readFileSync (req.file.path); fs.unlinkSync (req.file.path);
         const compressedName = path.join (data_images, uploadedName) + '.zz', compressedData = image_dataCompress (uploadedData);
         fs.writeFileSync (compressedName, compressedData);
-        console.log (`/images upload succeeded: '${uploadedName}' (${uploadedData.length} bytes) --> '${compressedName}' (${compressedData.length} bytes)`);
+        console.log (`/images upload succeeded: '${uploadedName}' (${uploadedData.length} bytes) --> '${compressedName}' (${compressedData.length} bytes) [${req.headers ['x-forwarded-for'] || req.connection.remoteAddress}]`);
         res.send ('File uploaded, compressed, and saved successfully.');
     } catch (error) {
-        console.error (`/images upload failed: error <<<${error}>>>`);
+        console.error (`/images upload failed: error <<<${error}>>> [${req.headers ['x-forwarded-for'] || req.connection.remoteAddress}]`);
         res.status (500).send ('File upload error');
     }
 });
@@ -97,7 +97,7 @@ xxx.get ('/images/:filename', (req, res) => {
     try {
         res.set ('Content-Type', 'application/octet-stream');
         res.send (fs.readFileSync (downloadPath));
-    	console.log (`/images download succeeded: ${downloadName} (${downloadPath})`);
+        console.log (`/images download succeeded: ${downloadName} (${downloadPath})`);
     } catch (error) {
         console.error (`/images download failed: ${downloadName} (${downloadPath}), error <<<${error}>>>`);
         res.status (404).send ('File not found');
